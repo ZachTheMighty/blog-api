a31ee971bf3f76a8be559f9791b41d593240c203
ZachTheMighty <promoted66@gmail.com>
2026-09-13 05:30:13 +0300

    add: error handling for creating a user with an already existing email

--- controllers/sign_up.js
+++ controllers/sign_up.js
@@ -66,16 +66,24 @@ const createUser = [
     if (!errors.isEmpty())
       return res.status(422).json({ error: errors.array() });
 
-    await prisma.user.create({
-      data: {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: await bcrypt.hash(matchedData(req).password, 10),
-        isAuthor: req.body.isAuthor === true,
-      },
-    });
-    res.json({ message: "User created successfully" });
+    try {
+      await prisma.user.create({
+        data: {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: await bcrypt.hash(matchedData(req).password, 10),
+          isAuthor: req.body.isAuthor === true,
+        },
+      });
+      res.json({ message: "User created successfully" });
+    } catch (error) {
+      if (error.code === "P2002")
+        return res
+          .status(409)
+          .json({ error: "An account with this email already exists." });
+      res.status(400).json(error);
+    }
   },
 ];
 
