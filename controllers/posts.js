c00f29e646cd1a4d03d07cbd432b26a4af2f7f4c
ZachTheMighty <promoted66@gmail.com>
2026-09-15 15:46:10 +0300

    fix: name of variable of updatedPost

--- controllers/posts.js
+++ controllers/posts.js
@@ -44,11 +44,11 @@ const getPostById = async (req, res) => {
 };
 
 const incrementViews = async (req, res) => {
-  const updatedUser = await prisma.post.update({
+  const updatedPost = await prisma.post.update({
     where: { id: +req.params.id },
     data: { views: { increment: 1 } },
   });
-  res.json({ views: updatedUser.views });
+  res.json({ views: updatedPost.views });
 };
 
 const togglePublished = async (req, res) => {
