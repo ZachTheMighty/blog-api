b6d414f10880e7f354fb987bf3e1504233dc2599
ZachTheMighty <promoted66@gmail.com>
2026-09-16 11:44:00 +0300

    add: user object to comments in getPostById

--- controllers/posts.js
+++ controllers/posts.js
@@ -28,6 +28,7 @@ const getAllPosts = async (req, res) => {
     const comments = await prisma.comment.findMany({
       where: { postId: posts[i].id },
     });
+
     result[i] = posts[i];
     result[i].comments = comments;
   }
@@ -43,6 +44,12 @@ const getPostById = [
     const comments = await prisma.comment.findMany({
       where: { postId: post.id },
     });
+    for (let i = 0; i < comments.length; i++) {
+      const user = await prisma.user.findUnique({
+        where: { id: comments[i].userId },
+      });
+      comments[i].user = user;
+    }
     post.comments = comments;
     res.json(post);
   },
