d4db3f927c8b62c787c7b53c0effea1c87c6c0b8
ZachTheMighty <promoted66@gmail.com>
2026-09-17 11:55:30 +0300

    refactor: returned newly created or edited post with response json

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,14 +8,24 @@ const createPost = [
   authenticateUser,
   validatePost,
   async (req, res) => {
-    await prisma.post.create({
+    const post = await prisma.post.create({
       data: {
         authorId: req.payload.user.id,
         title: req.body.title,
         body: req.body.body,
       },
     });
-    res.json({ message: "successfully created post" });
+    const comments = await prisma.comment.findMany({
+      where: { postId: post.id },
+    });
+    for (let i = 0; i < comments.length; i++) {
+      const user = await prisma.user.findUnique({
+        where: { id: comments[i].userId },
+      });
+      comments[i].user = user;
+    }
+    post.comments = comments;
+    res.json({ post, message: "successfully created post" });
   },
 ];
 
@@ -97,14 +107,24 @@ const editPostById = [
   authenticateUser,
   validatePost,
   async (req, res) => {
-    await prisma.post.update({
+    const post = await prisma.post.update({
       where: { id: +req.params.id },
       data: {
         title: req.body.title,
         body: req.body.body,
       },
     });
-    res.json({ message: "successfully edited post" });
+    const comments = await prisma.comment.findMany({
+      where: { postId: post.id },
+    });
+    for (let i = 0; i < comments.length; i++) {
+      const user = await prisma.user.findUnique({
+        where: { id: comments[i].userId },
+      });
+      comments[i].user = user;
+    }
+    post.comments = comments;
+    res.json({ post, message: "successfully edited post" });
   },
 ];
 
