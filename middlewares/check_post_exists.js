3b46cf1c6fa253e43bd1ce17b046f72baa0ce483
ZachTheMighty <promoted66@gmail.com>
2026-09-18 11:07:46 +0300

    change: checkPostExists() no longer attaches post to request object since it's not used by anyone

--- middlewares/check_post_exists.js
+++ middlewares/check_post_exists.js
@@ -1,9 +1,9 @@
 const prisma = require("../lib/prisma.ts");
+
 module.exports = async (req, res, next) => {
   const post = await prisma.post.findUnique({
     where: { id: +req.params.id },
   });
   if (!post) return res.status(404).json({ error: "Post doesn't exist" });
-  req.post = post;
   next();
 };
